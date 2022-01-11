851469570c99ac41100c9b0865e48df90e17b6bc
Mohammed Mansur <95575195+mohammedmansur@users.noreply.github.com>
2022-01-11 18:17:30 +0300

    chat screen

--- bus_station/lib/Src/Home/Home.dart
+++ bus_station/lib/Src/Home/Home.dart
@@ -1,5 +1,9 @@
+import 'package:bus_station/Src/Service/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
+import 'general_user.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,25 +13,109 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _formGlobalKey = GlobalKey<FormState>();
+  final TextEditingController _chatController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    int id = 0;
+    var _authProvider = Provider.of<AuthService>(context);
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        title: const Text('Chat Box'),
+        centerTitle: true,
+        actions: [
+          CircleAvatar(
+            backgroundImage: AssetImage("assets/pn.jpg"),
+          )
+        ],
+      ),
       body: Stack(
         children: <Widget>[
-          ListView.builder(
-            //itemCount: messages.length,
-            shrinkWrap: true,
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Container(
-                padding:
-                    EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
-                // child: Text(messages[index].messageContent),
-              );
-            },
-          ),
+          Container(
+              height: double.infinity,
+              child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('1234')
+                      .orderBy('Time')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('error');
+                    } else if (!snapshot.hasData || snapshot.data == null) {
+                      return Text('empty');
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return LinearProgressIndicator();
+                    }
+
+                    List<DocumentSnapshot> _docs = snapshot.data!.docs;
+
+                    List<GeneralUser> messages = _docs
+                        .map((e) => GeneralUser.fromMap(
+                            e.data() as Map<String, dynamic>))
+                        .toList();
+
+                    return ListView.builder(
+                      itemCount: messages.length,
+                      shrinkWrap: true,
+                      padding: EdgeInsets.only(top: 5, bottom: 5),
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.only(
+                              left: 14, right: 5, top: 5, bottom: 5),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                    child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.grey,
+                                  ),
+                                  padding: EdgeInsets.all(10),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        messages[index].chat ?? 'no chat',
+                                        maxLines: 1,
+                                        overflow: TextOverflow.clip,
+                                        softWrap: true,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      messages[index].date == null
+                                          ? Container()
+                                          : Text(
+                                              messages[index]
+                                                  .date!
+                                                  .toDate()
+                                                  .minute
+                                                  .toString(),
+                                              style: TextStyle(
+                                                fontSize: 10,
+                                              ),
+                                            ),
+                                    ],
+                                  ),
+                                )),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  })),
           Align(
             alignment: Alignment.bottomLeft,
             child: Container(
@@ -37,27 +125,9 @@ class _HomeScreenState extends State<HomeScreen> {
               color: Colors.white,
               child: Row(
                 children: <Widget>[
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlue,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
                   Expanded(
                     child: TextField(
+                      controller: _chatController,
                       decoration: InputDecoration(
                           hintText: "Write message...",
                           hintStyle: TextStyle(color: Colors.black54),
@@ -68,7 +138,21 @@ class _HomeScreenState extends State<HomeScreen> {
                     width: 15,
                   ),
                   FloatingActionButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      await FirebaseFirestore.instance.collection('1234').add({
+                        'id': id.toString(),
+                        'chat': _chatController.value.text,
+                        'Time': Timestamp.now(),
+                      });
+                      id++;
+                      _chatController.clear();
+                      //     .collection('users')
+                      //     .add({
+                      //   'name': "wha",
+                      //   'email': 00121212,
+                      //   'bootCampId': "whad",
+                      // }).catchError((e) => debugPrint(e.toString()));
+                    },
                     child: Icon(
                       Icons.send,
                       color: Colors.white,

4207fb04fedf9f6ad8b1dfb78769dee2756c7284
Mohammed Mansur <95575195+mohammedmansur@users.noreply.github.com>
2022-01-25 00:52:54 +0300

    user profile

---
+++ lib/Src/Student/GeneraL_Pro.dart
@@ -0,0 +1,8 @@
+class Student {
+  final String name;
+  final String username;
+  final String image;
+  bool isFollowedByMe;
+
+  Student(this.name, this.username, this.image, this.isFollowedByMe);
+}

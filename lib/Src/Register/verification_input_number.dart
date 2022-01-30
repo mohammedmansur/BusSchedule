a21423273c655c6a49ead43e7e9687e892c6d94f
mstaf <mstafa.00857387@gmail.com>
2022-01-31 01:25:25 +0300

    create UI for verification Screen

---
+++ lib/Src/Register/verification_input_number.dart
@@ -0,0 +1,106 @@
+// ignore_for_file: prefer_const_constructors, camel_case_types
+
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class phoneNumberVerification extends StatefulWidget {
+  const phoneNumberVerification({Key? key}) : super(key: key);
+
+  @override
+  _phoneNumberVerificationState createState() =>
+      _phoneNumberVerificationState();
+}
+
+class _phoneNumberVerificationState extends State<phoneNumberVerification> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Iconsax.arrow_left4,
+          ),
+          iconSize: 30,
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        elevation: 0.0, // remove shadow of appbar
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black, // change color of back icon
+
+        actions: [
+          SizedBox(
+            // this is for shift to left
+            width: 100,
+            child: IconButton(
+              icon: const Icon(Icons.home_rounded),
+              tooltip: 'go to home',
+              color: Colors.black,
+              iconSize: 34,
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 250, bottom: 8),
+            child:  Text(
+            "please enter your phone No.",
+            style: TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
+          ),
+         
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 60, right: 60),
+            child: TextField(
+              
+               controller: TextEditingController(text: "+964", ),
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.only(left: 20),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey, width: 2),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                floatingLabelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.black, width: 1.5),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40),
+            child: MaterialButton(
+              onPressed: () {
+                setState(() {});
+              },
+              height: 45,
+              color: Colors.blueGrey,
+              child: const Text(
+                "verify",
+                style: TextStyle(color: Colors.white, fontSize: 16.0),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
